d4f2a167218786baa49a2e1b67e0b9ed4faa3cc4
mcrkgus <mcrkgus@naver.com>
2024-11-19 02:56:07 +0900

    [Feat] Base Code (#8)

--- LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/View/ChatCertificationView.swift
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/View/ChatCertificationView.swift
@@ -5,3 +5,40 @@
 //  Created by Gahyun Kim on 11/19/24.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChatCertificationView: UIView {
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+extension ChatCertificationView {
+
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setStyle() {
+        
+    }
+}
--- LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/ViewController/ChatCertificationViewController.swift
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/ViewController/ChatCertificationViewController.swift
@@ -4,4 +4,43 @@
 //
 //  Created by Gahyun Kim on 11/19/24.
 //
+import UIKit
 
+import SnapKit
+import Then
+
+final class ChatCertificationViewController: UIViewController {
+    
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+}
+
+extension ChatCertificationViewController {
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setStyle() {
+        
+    }
+}
