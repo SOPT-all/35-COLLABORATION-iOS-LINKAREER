d7e0903661094cd82aef2b5f8c1ce1e729c77565
kms0233 <cnblue789@naver.com>
2024-11-20 14:05:51 +0900

    [Feat] HomeModel 구현 (#11)

---
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/Home/Model/HomeModel.swift
@@ -0,0 +1,34 @@
+//
+//  HomeModel.swift
+//  LINKAREER-iOS
+//
+//  Created by 김민서 on 11/19/24.
+//
+
+import UIKit
+
+struct HomeBanner {
+    let image: UIImage
+    let company: String
+    let title: String
+    let benefit: String
+    let views: Int
+    let comment: Int
+}
+
+struct TagHeader {
+    let nickname: String
+    let title: String
+    let discription: String
+    let labels: [String]
+}
+
+struct Board {
+    let category: String
+    let title: String
+    let content: String
+    let image: UIImage
+}
+
+
+
