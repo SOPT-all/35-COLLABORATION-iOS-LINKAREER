74fd19dbd0283a567e4327e4a9688cbd2b9ee0b9
mcrkgus <mcrkgus@naver.com>
2024-11-20 16:22:27 +0900

    [Feat] ChatCertificationInitialVC collectionview (#8)

--- LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/Initial/ViewController/ChatCertificationInitialViewController.swift
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/Initial/ViewController/ChatCertificationInitialViewController.swift
@@ -11,7 +11,11 @@ import Then
 
 final class ChatCertificationInitialViewController: UIViewController {
     
-
+    // MARK: - Properties
+    
+    private let rootView = ChatCertificationInitialView()
+    var dummy = CertificationInitialList.dummy()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -20,27 +24,49 @@ final class ChatCertificationInitialViewController: UIViewController {
         
         setHierarchy()
         setLayout()
-        setStyle()
+        
+        setDelegate()
+        setRegister()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
     }
-    
 }
 
-extension ChatCertificationInitialViewController {
-    
+private extension ChatCertificationInitialViewController {
     func setHierarchy() {
-        
+        view.addSubview(rootView)
     }
     
     func setLayout() {
-        
+        rootView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
-    func setStyle() {
-        
+    func setDelegate() {
+        rootView.listCollectionView.delegate = self
+        rootView.listCollectionView.dataSource = self
+    }
+    
+    func setRegister() {
+        rootView.listCollectionView.register(ChatCertificationInitialCell.self,
+                                             forCellWithReuseIdentifier: ChatCertificationInitialCell.identifier)
     }
 }
+
+extension ChatCertificationInitialViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummy.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatCertificationInitialCell", for: indexPath) as? ChatCertificationInitialCell else { return UICollectionViewCell() }
+        let item = dummy[indexPath.item]
+        cell.configure(with: item)
+        return cell
+    }
+}
+
+extension ChatCertificationInitialViewController: UICollectionViewDelegate {}
