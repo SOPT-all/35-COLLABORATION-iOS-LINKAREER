64db011e3c32dd171eaae91dd4a072e720cc81f7
mcrkgus <mcrkgus@naver.com>
2024-11-21 00:06:16 +0900

    [Feat] ChatCertification procedure View, VC Base (#16)

---
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/Procedure/View/ChatCertificationProcedureView.swift
@@ -0,0 +1,44 @@
+//
+//  ChatCertificationProcedureView.swift
+//  LINKAREER-iOS
+//
+//  Created by Gahyun Kim on 11/21/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class ChatCertificationProcedureView: UIView {
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+extension ChatCertificationProcedureView {
+
+    func setHierarchy() {
+
+    }
+
+    func setLayout() {
+
+    }
+
+    func setStyle() {
+
+    }
+}
---
+++ LINKAREER-iOS/LINKAREER-iOS/Presentation/ChatCertification/Procedure/ViewController/ChatCertificationProcedureViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ChatCertificationProcedureViewController.swift
+//  LINKAREER-iOS
+//
+//  Created by Gahyun Kim on 11/21/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class ChatCertificationProcedureViewController: UIViewController {
+
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+
+}
+
+extension ChatCertificationProcedureViewController {
+
+    func setHierarchy() {
+
+    }
+
+    func setLayout() {
+
+    }
+
+    func setStyle() {
+
+    }
+}
