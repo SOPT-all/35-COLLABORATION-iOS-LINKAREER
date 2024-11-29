a1b089b73d487154b03ed93ef6672c067588586b
mcrkgus <mcrkgus@naver.com>
2024-11-29 16:33:58 +0900

    [Network] Bookmark ResponseDTO (#41)

---
+++ LINKAREER-iOS/LINKAREER-iOS/Network/NewbieNetwork/BookMarkResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  BookMarkResponseDTO.swift
+//  LINKAREER-iOS
+//
+//  Created by Gahyun Kim on 11/28/24.
+//
+
+import Foundation
+
+struct BookmarkResponseDTO: Codable {
+    let message: String
+}
