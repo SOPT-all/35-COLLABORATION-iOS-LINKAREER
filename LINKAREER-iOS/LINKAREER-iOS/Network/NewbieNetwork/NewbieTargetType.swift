10cae60ebbdfe3f9f9df6ba25a99432b4354a6bc
mcrkgus <mcrkgus@naver.com>
2024-11-29 16:34:44 +0900

    [Network] Bookmark Target Type(#41)

--- LINKAREER-iOS/LINKAREER-iOS/Network/NewbieNetwork/NewbieTargetType.swift
+++ LINKAREER-iOS/LINKAREER-iOS/Network/NewbieNetwork/NewbieTargetType.swift
@@ -10,12 +10,20 @@ import Moya
 enum NewbieTargetType {
     // 서버에 요청할 API를 정의한 열거형
     case getPostList(category: String)
+    case postBookMark(officialId: Int)
+    case deleteBookMark(officialId: Int)
 }
 
 extension NewbieTargetType: BaseTargetType {
     var utilPath: String {
-        // 게시물 API의 기본 경로
-        return "api/v1/officiallist"
+        switch self {
+        case .getPostList(let category):
+            return "api/v1/officiallist"
+        case .postBookMark(let officialId):
+            return "api/v1/bookmark"
+        case .deleteBookMark(let officialId):
+            return "api/v1/bookmark"
+        }
     }
     
     // 요청별 상세 경로
@@ -23,12 +31,23 @@ extension NewbieTargetType: BaseTargetType {
         switch self {
         case .getPostList(let category):
             return utilPath
+        case .postBookMark(let officialId):
+            return "\(utilPath)/\(officialId)"
+        case .deleteBookMark(let officialId):
+            return "\(utilPath)/\(officialId)"
         }
     }
     
     // HTTP 메서드 설정
     var method: Moya.Method {
-        return .get
+        switch self {
+        case .getPostList(let category):
+            return .get
+        case .postBookMark(let officialId):
+            return .post
+        case .deleteBookMark(let officialId):
+            return .delete
+        }
     }
     
     var task: Moya.Task {
@@ -36,8 +55,13 @@ extension NewbieTargetType: BaseTargetType {
             // 서버로 전달할 파라미터와 인코딩 설정
         case .getPostList(let category):
             return .requestParameters(parameters: ["category": category], encoding: URLEncoding.default)
+        case .postBookMark:
+            return .requestPlain
+        case .deleteBookMark:
+            return .requestPlain
         }
     }
+    
     // 요청 헤더 설정
     var headers: [String: String]? {
         return [
